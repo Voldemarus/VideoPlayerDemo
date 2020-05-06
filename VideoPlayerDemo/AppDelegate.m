c297097dd96cf6f1a419470765f18b93135cd0b6
Vladimir Vodolazkiy <vloldemarus@gmail.com>
2020-05-06 08:57:47 +0300

    Simple AVPlayer with loop support implemented.

--- VideoPlayerDemo/AppDelegate.m
+++ VideoPlayerDemo/AppDelegate.m
@@ -7,8 +7,18 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
 @interface AppDelegate ()
+{
+    AVPlayer *player;
+    BOOL loopPlayer;
+}
+@property (weak) IBOutlet AVPlayerView *playerView;
+@property (weak) IBOutlet NSButton *loopCheckbox;
+@property (weak) IBOutlet NSButton *startButton;
+@property (weak) IBOutlet NSButton *stopButton;
 
 @property (weak) IBOutlet NSWindow *window;
 @end
@@ -16,13 +26,61 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
+
+    // init buttons state
+    self.startButton.enabled = YES;
+    self.stopButton.enabled = NO;
+    self.loopCheckbox.state = NSControlStateValueOff;
+    loopPlayer = NO;
+
+    // set up player
+    NSBundle *mb = [NSBundle mainBundle];
+    NSURL *demoURL = [mb URLForResource:@"demo" withExtension:@"mp4"];
+    if (demoURL) {
+        player = [[AVPlayer alloc] initWithURL:demoURL];
+        self.playerView.player = player;
+    } else {
+        NSLog(@"Cannot open demo.mp4!!!");
+    }
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(movieEndDetected:)
+               name:@"AVPlayerItemDidPlayToEndTimeNotification"
+             object:player.currentItem];
+ }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+- (IBAction)startButtonClicked:(id)sender
+{
+    self.startButton.enabled = NO;
+    self.stopButton.enabled = YES;
+    [player play];
+}
+
+- (IBAction)stopButtonClicked:(id)sender
+{
+    self.startButton.enabled = YES;
+    self.stopButton.enabled = NO;
+    [player pause];
+    [player seekToTime:kCMTimeZero];
+}
+
+- (IBAction)loopCheckBoxChanged:(id)sender
+{
+    loopPlayer = (self.loopCheckbox.state == NSControlStateValueOn);
+}
+
+- (void) movieEndDetected:(NSNotification *) note
+{
+    if (loopPlayer) {
+        [player seekToTime:kCMTimeZero];
+        [player play];
+    } else {
+        [self stopButtonClicked:nil];
+    }
+}
 
 @end
